a3fc51cc240976522179f5903847f53f1ab51847
sheepheeseok <heeseokid@g.shingu.ac.kr>
2025-01-03 18:39:53 +0900

    Home lifestyle 탭 추가

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import SwiperComponent from "../hooks/SwiperComponent.jsx";
+import LifestyleCard from "../cards/LifestyleCard.jsx";
 
 const Home = () => {
     return(
         <div className="home-container">
-            <div className="w-full xl:max-w-[1650px] md:max-w-[1090px] max-h-full">
+            <div className="mt-[140px] w-full xl:max-w-[1650px] md:max-w-[1090px]">
                 <SwiperComponent/>
+            </div>
+            <div className="lifestyle">
+                <a className="lifestyle-link">Lifestyle</a>
+                <a className="lifestyle-subtitle">프리미엄 라이프스타일의 완성</a>
+                <LifestyleCard />
             </div>
         </div>
     )
